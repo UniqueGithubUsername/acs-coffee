52c023371b38023112082de147e193c7a1138aa4
UniqueGithubUsername <greenglasses1@web.de>
2025-06-23 15:42:28 +0200

    update

--- acs_coffee_management/main/models.py
+++ acs_coffee_management/main/models.py
@@ -1,11 +1,24 @@
 from django.utils import timezone
 from django.db import models
 
+# Choices
+BUILDING_CHOICES = [
+    ('Main Building', 'Main Building'),
+	('Sense Building', 'Sense Building'),
+]
+
+TYPE_CHOICES = [
+    ('Hiwi', 'Hiwi'),
+	('Research Assistant', 'Research Assistant'),
+]
+
 # Create your models here.
 class Employee(models.Model):
 	name = models.CharField(max_length=255)
 	qr = models.SlugField(max_length=255, unique=True)
 	email = models.EmailField(max_length=255)
+	type = models.CharField(max_length=255, choices=TYPE_CHOICES, default='Research Assistant')
+	building = models.CharField(max_length=255, choices=BUILDING_CHOICES, default='Main Building')
 	debth = models.DecimalField(max_digits=6, decimal_places=2)
 	coffees = models.IntegerField(default=0)
 	updated_at = models.DateTimeField(auto_now=True)
