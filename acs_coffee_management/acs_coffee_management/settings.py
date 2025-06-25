6eecad73b7c56bd64ed64143d3008d40f3e7ca58
UniqueGithubUsername <greenglasses1@web.de>
2025-06-25 13:40:51 +0200

    Changes for deployment

--- acs_coffee_management/acs_coffee_management/settings.py
+++ acs_coffee_management/acs_coffee_management/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,11 +84,13 @@ WSGI_APPLICATION = 'acs_coffee_management.wsgi.application'
 #    }
 #}
 
+# Test
 # Default SQLite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'usr/share/acs-coffee/db.sqlite3',
+        'NAME': '/usr/share/acs-coffee/db.sqlite3',
+        #'NAME': 'db.sqlite3',
     }
 }
 
